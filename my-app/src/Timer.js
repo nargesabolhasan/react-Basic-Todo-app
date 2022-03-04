40a13fcfd6fb69d22928c2bd8f528ba94b126ff8
narges Hajabolhasan <narges.abolhasan@gmail.com>
2022-03-04 12:28:40 +0330

    todo app added

---
+++ my-app/src/Timer.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class Timer extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            time:new Date(),
+        }
+    }
+    setTime=()=>{
+        this.setState({time:new Date()})
+    }
+    componentDidMount(){
+        // this.interval = setInterval(() =>this.setTime(),1000)
+        this.timeOut = setInterval(() =>this.setTime(),1000)
+    }
+    componentDidUpdate(){
+        this.timeOut = setInterval(() =>this.setTime(),1000)
+    // componentWillUnmount(){
+    //     this.interval.clearInterval()
+    }
+    render() {
+        return (
+            <div>
+                {this.state.time.toLocaleTimeString()}
+            </div>
+        );
+    }
+}
+
+export default Timer;
